d851de9f51048db360b9b104380c6ce6c2316efb
Devid Farinelli <devid@Devids-MBP.lan>
2017-02-03 17:04:02 +0100

    Created dude and basic movement animations

---
+++ src/phaser/sprites/Dude.ts
@@ -0,0 +1,36 @@
+import * as Phaser from 'phaser'
+
+interface ISprite {
+  game: Phaser.Game
+  x: number
+  y: number
+  key?: string
+}
+
+export default class Dude extends Phaser.Sprite {
+
+  cursors: Phaser.CursorKeys
+
+  constructor ({ game, x, y, key}: ISprite) {
+    super(game, x, y, key)
+    this.anchor.setTo(0.5)
+    this.cursors = this.game.input.keyboard.createCursorKeys()
+  }
+
+  update() {
+    console.log(this)
+    if (this.cursors.left.isDown) {
+        // this.body.velocity.x = -150
+        this.animations.play('left')
+    } else if (this.cursors.right.isDown) {
+        //  Move to the right
+        // this.body.velocity.x = 150
+        this.animations.play('right')
+    } else {
+        //  Stand still
+        this.animations.stop()
+        this.frame = 4
+    }
+  }
+
+}
