1d089eb0410f0ad6a48049d92e409cf8c55cbd29
Alberto Nicoletti <albyx.n@gmail.com>
2017-02-06 20:53:43 +0100

    Added ace editor

---
+++ src/components/Editor.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import AceEditor from 'react-ace'
+import { css, StyleSheet } from 'aphrodite'
+
+import 'brace/mode/javascript'
+import 'brace/theme/github'
+
+const styles = StyleSheet.create({
+    editor: {
+        flex: 1
+    }
+})
+
+export interface EditorProps {
+    code: string
+}
+
+export default class Editor extends React.Component<EditorProps, undefined> {
+
+    onTextInput = (text: string) => {
+        console.log(text)
+    }
+
+    render() {
+        return (
+            <AceEditor
+                mode=''
+                theme='github'
+                onChange={this.onTextInput}
+                name='code-editor'
+                defaultValue={this.props.code}
+                editorProps={{$blockScrolling: Infinity}}
+                tabSize={4}
+                fontSize={16}
+                width={'auto'}
+                height={'auto'}
+                className={css(styles.editor)}
+            />
+        )
+    }
+
+}
--- src/components/Riddle.tsx
+++ src/components/Riddle.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { css, StyleSheet } from 'aphrodite'
 
+import Editor from './Editor'
+
 const khosrau = {
     fontFamily: 'Cuneiform',
     fontStyle: 'normal',
@@ -14,17 +16,72 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flex: 1,
     },
+    cuneiformSection: {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+    },
+    editorSection: {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+    },
+    separatorContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        width: 30,
+        border: 'black 1px inset',
+    },
+    legendContainer: {
+
+    },
     cuneiform: {
         fontFamily: khosrau,
-        fontSize: 50,
-    }
+        fontSize: 30,
+    },
 })
+
+export interface CuneiformSectionProps {
+    riddle: string
+    isLegendActive: boolean
+}
+
+const Legend = () =>
+    <div className={css(styles.legendContainer)}>
+        <hr />
+        <p>
+            Qui ci metteremo la legenda!
+        </p>
+    </div>
+
+const CuneiformSection = ({ riddle, isLegendActive }: CuneiformSectionProps) =>
+    <div className={css(styles.cuneiformSection)}>
+        <p className={css(styles.cuneiform)}>
+            {riddle}
+        </p>
+        {isLegendActive ? <Legend /> : null}
+    </div>
+
+const Separator = () =>
+    <div className={css(styles.separatorContainer)}>
+        <button>{'<'}</button>
+    </div>
+
+export interface EditorSectionProps {
+    code: string
+}
+
+const EditorSection = ({ code }: EditorSectionProps) =>
+    <div className={css(styles.editorSection)}>
+        <Editor code={code}/>
+    </div>
 
 const Riddle = () =>
     <div className={css(styles.riddleContainer)}>
-        <p className={css(styles.cuneiform)}>
-            Bonfante merda
-        </p>
+        <CuneiformSection riddle={'if (1 == 1)\n    return true'} isLegendActive/>
+        <Separator />
+        <EditorSection code={'if (1 == 1)\n    return true'}/>
     </div>
 
 export default Riddle
