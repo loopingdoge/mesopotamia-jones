402d59798ccf58110b890a71374e8da43a1de6b4
Devid Farinelli <devid.farinelli@gmail.com>
2017-02-16 20:11:31 +0100

    Aggiunta bozza di progressione di livello

--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -1,22 +1,40 @@
-import { observable, action, /* computed */ } from 'mobx'
+import { observable, action, reaction, /* computed */ } from 'mobx'
 import { RouterStore } from 'mobx-react-router'
+import { RiddleStore } from './riddleStore'
 
 export class GameStore {
+
+    init(stores: any) {
+        let { riddleStore, routingStore } = stores
+        this.push = routingStore.push
+        this.riddleStore = riddleStore
+
+        reaction(
+            () => this.riddleStore.isSolved,
+            () => this.levelSolved(this.riddleStore.level)
+        )
+    }
+
+    push: (path: string) => void
+    riddleStore: RiddleStore
 
     @observable level: number = 0
 
     @action incrementLevel = () => this.level++
 
     @action goToRiddle = (level: number) => {
-        this.level = level
+        this.riddleStore.changeLevel(this.level)
         this.push('/riddle')
     }
 
-    push: (path: string) => void
-
-    setRoutingStore(routingStore: RouterStore) {
-        this.push = routingStore.push
+    @action levelSolved = (level: number) => {
+        this.level = level+1
+        this.push('/game')
     }
+
+    
+
+    
 }
 
 const gameStore = new GameStore()
