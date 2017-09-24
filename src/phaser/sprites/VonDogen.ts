bb34098126e93ff81c20c9c08080da0c3676d2e2
Devid Farinelli <devid.farinelli@gmail.com>
2017-09-24 16:12:19 +0200

    [FIX] Wrong classname for VonDogen

--- src/phaser/sprites/VonDogen.ts
+++ src/phaser/sprites/VonDogen.ts
@@ -3,7 +3,7 @@ import { Game } from 'phaser-ce'
 import { GameState } from '../../stores/gameStore'
 import Npc from './Npc'
 
-export default class VonTalin extends Npc {
+export default class VonDogen extends Npc {
     constructor(game: Game, x: number, y: number) {
         super(game, x, y, 'doge')
         this.frame = 4
