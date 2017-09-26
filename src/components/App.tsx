bc39a851bb4fcf2160316f150f847ccd17c455c3
Alberto Nicoletti <albyx.n@gmail.com>
2017-09-26 12:46:25 +0200

    Added (but commented) Mobx <DevTools>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 declare const __DEV__: boolean
 import { css, StyleSheet } from 'aphrodite'
 import { inject, observer } from 'mobx-react'
+import DevTools from 'mobx-react-devtools'
 import * as React from 'react'
 import { Route, Switch } from 'react-router'
 
@@ -19,6 +20,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div className={css(styles.appContainer)}>
+                {/* <DevTools /> */}
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/game" component={MesopotamiaJones} />
