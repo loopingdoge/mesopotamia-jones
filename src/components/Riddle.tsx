dbb4ed7678280a2bfa299c883c66bb114b3158a5
Alberto Nicoletti <albyx.n@gmail.com>
2017-09-26 09:48:24 +0200

    Preven user selection in lockcode

--- src/components/Riddle.tsx
+++ src/components/Riddle.tsx
@@ -90,7 +90,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-around',
-        padding: 16
+        padding: 16,
+        userSelect: 'none'
     },
     solutionSection: {
         position: 'absolute',
