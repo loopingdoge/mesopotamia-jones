b6e1551727cc59f8c2b768b756904fc4d17ad685
Alberto Nicoletti <albyx.n@gmail.com>
2017-09-28 12:47:15 +0200

    Test working on React 16

--- src/components/__tests__/CuneiformChar.test.tsx
+++ src/components/__tests__/CuneiformChar.test.tsx
@@ -1,8 +1,12 @@
-import { shallow } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
 import {} from 'jest'
 import React from 'react'
 
 import CuneiformChar from '../CuneiformChar'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 jest.mock('aphrodite/lib/inject')
 
