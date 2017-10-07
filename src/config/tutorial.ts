358444d3405bb7c37b8719eca6269f63d39de9a9
Devid Farinelli <devid.farinelli@gmail.com>
2017-10-07 15:44:25 +0200

    Removes ellipsis from tutorial

--- src/config/tutorial.ts
+++ src/config/tutorial.ts
@@ -19,20 +19,20 @@ export interface Tutorial {
 export const tutorialSteps = (inventory: Inventory) => {
     const baseTutorial: Tutorial[] = [
         {
-            title: "L'indovinello...",
+            title: "L'indovinello",
             selector: '#cuneiformRiddle',
             text:
                 'Per aprire la porta devi risolvere questo indovinello.\n ATTENTO! I dati cambiano sempre'
         },
         {
-            title: "Tradurre l'indovinello...",
+            title: "Tradurre l'indovinello",
             selector: '#cuneiformRiddle',
             text:
                 'Selezionando una lettera puoi scoprire a quale simbolo corrisponde nella legenda',
             video: TranslateSrc as any
         },
         {
-            title: 'Aprire la porta...',
+            title: 'Aprire la porta',
             selector: '#lockcode',
             text:
                 "Per aprire la porta devi inserire qui la soluzione dell'indovinello",
@@ -41,21 +41,21 @@ export const tutorialSteps = (inventory: Inventory) => {
     ]
     const computerTutorial: Tutorial[] = [
         {
-            title: 'Il Computer...',
+            title: 'Il Computer',
             selector: '#blocklyArea',
             text:
                 'Il computer cerca di aprire la porta con la soluzione che hai inventato',
             video: ComputerSrc as any
         },
         {
-            title: 'I blocchi...',
+            title: 'I blocchi',
             selector: '.blocklyFlyout',
             text:
                 "Per creare una soluzione devi trascinare questi blocchi nell'area bianca ed unirli come i pezzi di un puzzle",
             video: BlocksSrc
         },
         {
-            title: 'Provare la soluzione...',
+            title: 'Provare la soluzione',
             selector: '#play',
             text:
                 'Questo pulsante esegue la tua soluzione. Con un risultato corretto si apre la porta'
