685c928c3070326efb0c0ed83f75ad81d7196959
Devid Farinelli <devid.farinelli@gmail.com>
2017-10-16 06:12:53 +0200

    Hammurtossi walks and dances

--- src/phaser/sprites/Hammurtosh.ts
+++ src/phaser/sprites/Hammurtosh.ts
@@ -3,9 +3,16 @@ import { Game } from 'phaser-ce'
 import { GameState } from '../../stores/gameStore'
 import Npc from './Npc'
 
+import { linearMap } from '../../utils'
+
 export default class Hammurtosh extends Npc {
     constructor(game: Game, x: number, y: number) {
         super(game, x, y, 'hammurtosh')
+
+        this.game.physics.enable(this)
+
+        this.animations.add('right', [8, 9, 10, 11], 10, true)
+        this.animations.add('idle', [1, 3], 2, true)
     }
 
     dialogue(state: GameState): string {
@@ -18,7 +25,8 @@ export default class Hammurtosh extends Npc {
     }
 
     moveTo(x: number, y: number) {
-        this.position.x = Math.floor(x)
-        this.position.y = Math.floor(y)
+        this.animations.play('right')
+        this.game.add.tween(this).to({ x, y }, 500, Phaser.Easing.Default, true)
+        setTimeout(() => this.animations.play('idle'), 500)
     }
 }
